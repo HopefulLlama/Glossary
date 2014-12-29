624f79e8496b8bdae47ba03f709f2a26980176fb
HopefulLlama <jonlaw10170@gmail.com>
2014-12-29 19:36:02 +0000

    - Card does not get added if the term is already existent

--- public/src/js/ui-handler.js
+++ public/src/js/ui-handler.js
@@ -17,12 +17,23 @@ function cardController($scope) {
   $scope.parsedJSON = {};
 
   $scope.addCard = function() {
-    var tags = $scope.tags.split(',');
-    var newCard = {"title": $scope.title, "desc": $scope.description, "tags": tags};
-    $scope.parsedJSON.cards.push(newCard);
-    angular.element('[data-ng-controller=cardController').scope().$apply();
+    var exists = false;
+    $scope.parsedJSON.cards.forEach(function(card) {
+      if(card.title === $scope.title){
+        exists = true;
+      }
+    });
 
-    sendData($scope.parsedJSON);
+    if(exists) {
+      alert("Already exists. This card will not be added.");
+      } else {
+      var tags = $scope.tags.split(',');
+      var newCard = {"title": $scope.title, "desc": $scope.description, "tags": tags};
+      $scope.parsedJSON.cards.push(newCard);
+      angular.element('[data-ng-controller=cardController').scope().$apply();
+
+      sendData($scope.parsedJSON);
+    }
 
     $scope.title = "";
     $scope.description = "";
